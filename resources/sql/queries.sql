3cc79a6f226dc73eb2202c4b2017b13dd7ea5018
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-03-27 09:57:21 +0100

    Updates queries for ingredients

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,21 +1,14 @@
--- :name create-user! :! :n
--- :doc creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+-- :name create-ingredient! :! :n
+-- :doc creates a new ingredient record
+INSERT INTO ingredients
+(name, created_at, updated_at)
+VALUES (:name, now(), now())
 
--- :name update-user! :! :n
--- :doc updates an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
+-- :name get-ingredient :? :1
+-- :doc retrieves an ingredient by its id
+SELECT * FROM ingredients
 WHERE id = :id
 
--- :name get-user :? :1
--- :doc retrieves a user record given the id
-SELECT * FROM users
-WHERE id = :id
-
--- :name delete-user! :! :n
--- :doc deletes a user record given the id
-DELETE FROM users
-WHERE id = :id
+-- :name get-ingredients :? :*
+-- :doc retrieves all ingredients
+SELECT * FROM ingredients

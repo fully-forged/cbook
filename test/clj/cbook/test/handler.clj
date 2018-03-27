ef2e5c73c840920b3679ca3592ab0912cc84b0e8
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-03-27 18:50:24 +0100

    Tests graphql endpoint

--- test/clj/cbook/test/handler.clj
+++ test/clj/cbook/test/handler.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [ring.mock.request :refer :all]
             [cbook.handler :refer :all]
+            [cheshire.core :refer [parse-string]]
             [mount.core :as mount]))
 
 (use-fixtures
@@ -11,6 +12,15 @@
                  #'cbook.handler/app)
     (f)))
 
+(def get-ingredients-payload
+  "{ GetIngredients {
+      id
+     }
+   }")
+
+(defn get-json-body [response]
+  (parse-string (:body response)))
+
 (deftest test-app
   (testing "main route"
     (let [response (app (request :get "/"))]
@@ -18,4 +28,9 @@
 
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
-      (is (= 404 (:status response))))))
+      (is (= 404 (:status response)))))
+
+  (testing "graphql endpoint"
+    (let [response (app (request :post "/api" get-ingredients-payload))]
+      (is (= ["data"] (keys (get-json-body response))))
+      (is (= 200 (:status response))))))

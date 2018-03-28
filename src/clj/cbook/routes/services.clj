22b5964becff4a85152f3e56e796c823e7c1b3b7
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-03-28 17:31:21 +0100

    Support multiple content-types (json, graphql)

--- src/clj/cbook/routes/services.clj
+++ src/clj/cbook/routes/services.clj
@@ -4,6 +4,7 @@
             [com.walmartlabs.lacinia :as lacinia]
             [clojure.data.json :as json]
             [clojure.edn :as edn]
+            [clojure.string :as str]
             [ring.util.http-response :refer :all]
             [compojure.api.sweet :refer :all]
             [cbook.db.core :as db]))
@@ -26,14 +27,47 @@
                          :create-ingredient! create-ingredient!})
       schema/compile))
 
-(defn handler [body]
-  (let [json-body (json/read-str body)
-        query (get json-body "query")]
-    {:status 200
-     :headers {"Content-Type" "application/json"}
-     :body (let [result (lacinia/execute compiled-schema query nil nil)]
-             (json/write-str result))}))
+(defn variable-map
+  "Reads the `variables` query parameter, which contains a JSON string
+  for any and all GraphQL variables to be associated with this request.
+  Returns a map of the variables (using keyword keys)."
+  [request]
+  (let [vars (get-in request [:query-params :variables])]
+    (if-not (str/blank? vars)
+      (json/read-str vars :key-fn keyword)
+      {})))
+
+(defn unwrap-query
+  [query content-type]
+  (case content-type
+    "application/json" (:query (json/read-str query :key-fn keyword))
+    "application/graphql" query))
+
+(defn extract-query
+  [request]
+  (case (:request-method request)
+    :get (get-in request [:query-params :query])
+    :post (slurp (:body request))
+    :else ""))
+
+(defn ^:private graphql-handler
+  "Accepts a GraphQL query via GET or POST, and executes the query.
+  Returns the result as text/json."
+  [compiled-schema]
+  (let [context {:cache (atom {})}]
+    (fn [request]
+      (let [vars (variable-map request)
+            query (-> request
+                      (extract-query)
+                      (unwrap-query (:content-type request)))
+            result (lacinia/execute compiled-schema query vars context)
+            status (if (-> result :errors seq)
+                     400
+                     200)]
+        {:status status
+         :headers {"Content-Type" "application/json"}
+         :body (json/write-str result)}))))
 
 (defapi service-routes
-  (POST "/api" [:as {body :body}]
-    (handler (slurp body))))
+  (let [query-handler (graphql-handler compiled-schema)]
+    (POST "/api" request (query-handler request))))
